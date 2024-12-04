4ad2744bc9b493b594237b606935f9e14f4e8df9
Shefat-Shohan <shohan.ks3@gmail.com>
2024-12-04 15:31:25 +0600

    Search in Rotated Sorted Array

---
+++ BinarySearch.js
@@ -0,0 +1,22 @@
+// Binary search Algorithm
+
+let arr = [-1, 0, 3, 4, 5, 9, 12];
+let target = 12;
+function binarySearch(arr, tar) {
+  let st = 0;
+  let end = arr.length - 1;
+  while (st <= end) {
+    let mid = st + Math.floor((end - st) / 2);
+    if (tar > arr[mid]) {
+      st = mid + 1; // 2nd hlf
+    } else if (tar < arr[mid]) {
+      end = mid - 1; // 1st half
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
+
+let ans = binarySearch(arr, target);
+console.log(arr[ans]);
---
+++ SearchInSortedArray.js
@@ -0,0 +1,35 @@
+let arr = [4, 5, 6, 7, 0, 1, 2];
+let target = 0;
+
+function search(arr, tar) {
+  // Time complexity O(logn)
+  let st = 0;
+  let end = arr.length - 1;
+
+  while (st <= end) {
+    let mid = Math.floor(st + (end - st) / 2); // Midpoint calculation
+    if (arr[mid] == tar) {
+      return mid;
+    }
+
+    if (arr[st] <= arr[mid]) {
+      // Left sorted
+      if (arr[st] <= tar && tar < arr[mid]) {
+        end = mid - 1;
+      } else {
+        st = mid + 1;
+      }
+    } else {
+      // Right sorted
+      if (arr[mid] < tar && tar <= arr[end]) {
+        st = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+}
+
+search(arr, target);
